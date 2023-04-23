ee3c9b39e44acac07957019bfeed5d20e101380b
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2023-04-23 19:43:13 -0300

    User and properties

--- course/src/main/java/edu/joseph/course/entites/User.java
+++ course/src/main/java/edu/joseph/course/entites/User.java
@@ -1,10 +1,16 @@
 package edu.joseph.course.entites;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_users")
 public class User  implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
